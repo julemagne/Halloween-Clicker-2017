9199b9267d710134364194f30381ba9f5c07e3e0
Julie David <julie.angela.david@gmail.com>
2017-01-05 02:58:53 -0500

    Add surprise audio

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  scream:HTMLAudioElement;
 
   constructor() { }
 
   ngOnInit() {
+    this.scream = new Audio('../assets/audio/scream.mp3');
+  }
+
+  playScream = function() {
+    this.scream.play();
   }
 
   tiles: any[] = [
