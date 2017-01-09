325e9b94ee4c8c71ece18c875436703056292780
Julie David <julie.angela.david@gmail.com>
2017-01-09 12:18:35 -0500

    Add click cycle function to show each monster sequentially and router

--- src/app/monsters/monsters.component.ts
+++ src/app/monsters/monsters.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { Monster } from './shared/monster.model';
 import { MonsterService } from './shared/monster.service';
@@ -13,12 +14,29 @@ import { MonsterService } from './shared/monster.service';
 export class MonstersComponent implements OnInit {
   
   monsters: Monster[] = [];
+  monster: Monster = new Monster;
 
-  constructor(private monsterService: MonsterService) { }
+  constructor(
+    private monsterService: MonsterService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
     this.monsterService.getMonsters()
-      .then(monsters => this.monsters = monsters);
+      .then(monsters => {
+        this.monsters = monsters
+        this.monster = this.monsters[0]
+      });
+  }
+
+  cycle() {
+    let i = this.monsters.indexOf(this.monster);
+    let monstersLength = this.monsters.length-1;
+    if (i != monstersLength) {
+      this.monster = this.monsters[i+1];
+    } else {
+      this.router.navigate(['/home']);
+    }
   }
 
 }
